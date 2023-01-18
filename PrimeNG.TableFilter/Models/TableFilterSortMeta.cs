5fed62344b6cacbf472bd6910780bad9b8d8b63d
Mostafa Esmaeili <mostafa.esmaeili8@gmail.com>
2023-01-19 00:43:29 +0330

    Add .gitignore and remove tracked files

--- PrimeNG.TableFilter/Models/TableFilterSortMeta.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace PrimeNG.TableFilter.Models
-{
-    public class TableFilterSortMeta
-    {
-        public string Field { get; set; }
-        public int Order { get; set; }
-    }
-}
