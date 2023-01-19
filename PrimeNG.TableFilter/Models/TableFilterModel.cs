2aa4eb61cd3a44125748885b7cd4aedd3c7c09a4
Mostafa Esmaeili <mostafa.esmaeili8@gmail.com>
2023-01-19 11:26:13 +0330

    add default value to TableFilterModel

--- PrimeNG.TableFilter/Models/TableFilterModel.cs
+++ PrimeNG.TableFilter/Models/TableFilterModel.cs
@@ -6,9 +6,9 @@ namespace PrimeNG.TableFilter.Models
     public class TableFilterModel
     {
         public Dictionary<string, object> Filters { get; set; } = new Dictionary<string, object>();
-        public int First { get; set; }
-        public int Rows { get; set; }
-        public string SortField { get; set; }
+        public int First { get; set; } = 0;
+        public int Rows { get; set; } = 1000;
+        public string? SortField { get; set; }
         public int SortOrder { get; set; }
         public List<TableFilterSortMeta> MultiSortMeta { get; set; } = new List<TableFilterSortMeta>();
     }
